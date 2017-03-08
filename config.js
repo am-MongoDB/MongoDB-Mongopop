c32f78fca7faf4cbdb26c720d269973c8fe9503a
Andrew Morgan <andrew.morgan@mongodb.com>
2017-03-08 13:52:31 +0000

    cleanup

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var config = {
 		},
 		mockarooUrl: "http://www.mockaroo.com/536ecbc0/download?count=1000&key=48da1ee0"
 	},
-	makerMongoDBURI: "mongodb://billy:goldie@cluster0-shard-00-00-qfovx.mongodb.net:27017,cluster0-shard-00-01-qfovx.mongodb.net:27017,cluster0-shard-00-02-qfovx.mongodb.net:27017/maker?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+	makerMongoDBURI: "mongodb://localhost:27017/maker?authSource=admin",
 	checkinCollection: "foursq"
 };
 
